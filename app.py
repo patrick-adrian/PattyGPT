5684a7c786eac58455a558db381ec7eab97c71ec
Patrick Pineda <patrickpineda@Jacks-MacBook-Air.local>
2025-07-25 12:03:13 -0400

    fix for render port

--- app.py
+++ app.py
@@ -93,4 +93,7 @@ def sms_reply():
     return str(twilio_response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
